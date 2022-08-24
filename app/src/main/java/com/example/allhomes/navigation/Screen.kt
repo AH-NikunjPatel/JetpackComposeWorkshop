a14fad6c92a4074409fe34309e863244ccabcda8
AH-NikunjPatel <nikunj.patel@domain.com.au>
2022-08-24 17:12:41 +1000

    login screen implemented

--- app/src/main/java/com/example/allhomes/navigation/Screen.kt
+++ app/src/main/java/com/example/allhomes/navigation/Screen.kt
@@ -2,11 +2,13 @@ package com.example.allhomes.navigation
 
 enum class ScreenName(name: String){
     WELCOME("Welcome"),
+    LOGIN("Login"),
     DASHBOARD("Dashboard"),
     DETAIL("Detail")
 }
 sealed class Screen(val route: String){
     object WelcomeScreen : Screen(ScreenName.WELCOME.name)
+    object LoginScreen : Screen(ScreenName.LOGIN.name)
     object DashboardScreen : Screen(ScreenName.DASHBOARD.name)
     object DetailScreen : Screen(ScreenName.DETAIL.name)
 }
